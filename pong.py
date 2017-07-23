faf94b08411996c6c023107533a20e08ab754349
R.J Lélis <renatojlelis@gmail.com>
2017-07-22 21:28:05 -0300

    Colisão da bola nas bordas feita
    Adição das colisões, dos placares, remoção das imagens e uma refatoração do código em geral

--- pong.py
+++ pong.py
@@ -1,7 +1,6 @@
 import pygame
 from pygame.locals import *
-from sys import exit
-from random import randrange
+import colors
 
 pygame.init()
 
@@ -9,60 +8,95 @@ screen = pygame.display.set_mode((956, 560), 0, 32)
 
 pygame.display.set_caption('Pong')
 
-background_filename = 'cross.png'
+font = pygame.font.SysFont('Calibri', 50, True, False)
 
-background = pygame.image.load(background_filename).convert()
-
-player1 = {
-    'surface': pygame.image.load('player.png').convert_alpha(),
+player1 = { # Atributos do jogador á esquerda
+    'surface': [20, 99],
     'position': [5, 210],
     'speed': {
         'x': 0,
         'y': 0
-        }
+        },
+    'score': 0
     }
 
-player2 = {
-    'surface': pygame.image.load('player.png').convert_alpha(),
-    'position': [925, 210],
+player2 = { # Atributos do jogador á direita
+    'surface': [20, 99],
+    'position': [930, 210],
     'speed': {
         'x': 0,
         'y': 0
+        },
+    'score': 0
+    }
+
+ball = { # atributos da "bola"
+    'surface': [20, 25],
+    'position':[478, 210],
+    'speed': {
+        'x': 5,
+        'y': 5
         }
     }
 
 clock = pygame.time.Clock()
 
-standart_speed = 7
+stantart_player_speed = 7 # velocidade dos jogadores
 
-while True:
+done = False
+
+while not done:
 
     player1['speed']['y'] = 0
     player2['speed']['y'] = 0
 
     for event in pygame.event.get():
-        if event.type == QUIT:
-            exit()
-
+        if event.type == pygame.QUIT:
+            done = True
+            
+    # ---- Movimentação dos jogadores ---- #
+    
     pressed_key = pygame.key.get_pressed()
 
     if pressed_key[K_w]:
-        player1['speed']['y'] = standart_speed * (-1)
+        player1['speed']['y'] = stantart_player_speed * (-1)
     elif pressed_key[K_s]:
-        player1['speed']['y'] = standart_speed
+        player1['speed']['y'] = stantart_player_speed
 
     if pressed_key[K_UP]:
-        player2['speed']['y'] = standart_speed * (-1)
+        player2['speed']['y'] = stantart_player_speed * (-1)
     elif pressed_key[K_DOWN]:
-        player2['speed']['y'] = standart_speed
+        player2['speed']['y'] = stantart_player_speed
 
     player1['position'][1] += player1['speed']['y']
     player2['position'][1] += player2['speed']['y']
 
-    screen.blit(background, (0, 0))
+    # ---- Desenhos que aparecem na tela ---- #
+    
+    screen.fill(colors.BLACK)
 
-    screen.blit(player1['surface'], player1['position'])
-    screen.blit(player2['surface'], player2['position'])
+    score1 = font.render(str(player1['score']), True, colors.WHITE)
+    screen.blit(score1, [20, 20])
+
+    score2 = font.render(str(player2['score']), True, colors.WHITE)
+    screen.blit(score2, [910, 20])
+
+    pygame.draw.line(screen, colors.BLUE, [478,0], [478,560], 5)
+    
+    pygame.draw.rect(screen, colors.WHITE, [ball['position'], ball['surface']])
+    #ball['position'][0] += ball['speed']['x']
+    #ball['position'][1] += ball['speed']['y']
+
+    pygame.draw.rect(screen, colors.WHITE,[player1['position'], player1['surface']])
+    pygame.draw.rect(screen, colors.WHITE, [player2['position'], player2['surface']])
+
+    # ---- Colisões ---- #
+   
+    if ball['position'][1] > 535 or ball['position'][1] < 0:
+        ball['speed']['y'] *= -1
 
     pygame.display.update()
-    time_passed = clock.tick(30)
+    
+    clock.tick(30)
+    
+pygame.quit()
